438166c039843a48df8c899a6be582f63dbced1c
OliveMonk <gp7.official@gmail.com>
2023-09-27 23:21:22 +0530

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import Feed from "@components/Feed";
+import Feed from "../components/Feed";
 
 const Home = () => (
   <section className="w-full flex-center flex-col">
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/Profile";
+import Profile from "../../components/Profile";
 
 const MyProfile = () => {
   const router = useRouter();
