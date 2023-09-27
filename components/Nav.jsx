d5297c962f1e75ef5a0f749801986084002a15a0
OliveMonk <gp7.official@gmail.com>
2023-09-27 22:41:16 +0530

    update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -30,7 +30,7 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">Promptopia</p>
+        <p className="logo_text">PromptSocial</p>
       </Link>
 
       {/* Desktop Navigation */}
